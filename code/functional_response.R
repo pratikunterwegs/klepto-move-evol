4278598a346aa34c6fcf804c78046939083129ed
P.R. Gupte <p.r.gupte@rug.nl>
2020-11-26 13:41:34 +0100

    generalise function

--- code/functional_response.R
+++ code/functional_response.R
@@ -19,101 +19,54 @@ floor_any <- function(x, v) {
 #' @export
 #' 
 get_functional_response <-
-  function(
-           data_folder,
-           which_gen = seq(991, 998, 1),
-           n_time = 400,
-           layers = c(
+  function(layers = c(
              "items", "foragers", "klepts",
              "klepts_intake", "foragers_intake"
-           )) {
-
-    # list files
-    data_files <- data.table::CJ(
-      which_gen,
-      layers
-    )
-    data_files$filepath <- glue::glue_data(
-      .x = data_files,
-      "{data_folder}/{which_gen}{layers}.txt"
-    )
-    # split by layer
-    data_files <- split(data_files, data_files$layers)
-
-    # get only filepaths
-    data_files <- lapply(data_files, `[[`, "filepath")
-
-    # now read in all files as matrices
-    data_in <- rapply(object = data_files, function(file_list) {
-      matrices <- lapply(as.list(file_list), function(fl) {
-        tseries::read.matrix(fl)
-      })
-
-      # sum the agents over the generations 991 -- 998
-      matrices <- Reduce(f = `+`, x = matrices)
-    }, how = "list")
-
-    # convert to dataframe for capacity wise mean
-    data_proc <- rapply(data_in, function(matrix_) {
-      vals <- as.vector(matrix_) / length(which_gen) # for N gen mean
-      vals <- vals / n_time # for timestep mean
-
-      return(vals)
-    }, how = "list")
-
-    # get per capita forager intake
-    pc_intake_forager <- data_proc[["foragers_intake"]] /
-      data_proc[["foragers"]]
-
-    # get per capita klepto intake
-    pc_intake_klepts <- data_proc[["klepts_intake"]] /
-      data_proc[["klepts"]]
+           ),
+           response = c("pc_intake_forager",
+                        "pc_intake_klepts",
+                        "pc_intake_total"),
+           drivers = c("klepts",
+                       "foragers",
+                       "total_agents",
+                       "items"),
+           data_folder,
+           which_gen = seq(991, 998, 1),
+           n_time = 400) {
+  
+    # assert that drivers are only two
+    assertthat::assert_that(length(drivers) == 2,
+                            msg = "only two drivers allowed")
     
-    # replace NANs with 0
-    pc_intake_forager[is.nan(pc_intake_forager)] <- 0
-    pc_intake_klepts[is.nan(pc_intake_klepts)] <- 0
+    data_proc <- do_read_data(
+      data_folder,
+      which_gen = seq(991, 998, 1),
+      n_time = 400
+    )
     
-    # total pc intake
-    pc_intake_total <- pc_intake_forager + pc_intake_klepts
-
-    # add pc intake to list
-    data_proc <- append(data_proc, list(
-      pc_intake_forager = pc_intake_forager,
-      pc_intake_klepts = pc_intake_klepts,
-      pc_intake_total = pc_intake_total
-    ))
-
-    #### get pc intake per items and per agents ####
-    # first make data.table
-    data_proc <- as.data.table(data_proc)
-
-    # get total agents
-    data_proc[, `:=`(total_agents = foragers + klepts)]
 
     # floor agents and items to the nearest 0.01
     data_proc[, `:=`(total_agents = floor_any(total_agents, 0.005),
                      items = floor_any(items, 0.005))]
     
+    # subset data for driver and response columnsd
+    cols <- c(response, drivers)
+    data_fun_response <- data_proc[, ..cols]
+    
     # melt here before NANs appear
-    data_fun_response <- melt(data_proc, id.vars = setdiff(
-      colnames(data_proc),
-      c("pc_intake_forager", "pc_intake_klepts", "pc_intake_total")
+    data_fun_response <- melt(data_fun_response, id.vars = setdiff(
+      colnames(data_fun_response),
+      response
     ))
 
     # get intake rate
     data_fun_response <-
       data_fun_response[, lapply(.SD, mean),
         .SD = c("value"),
-        by = c("items", "total_agents", "variable")
+        by = c(drivers, "variable")
       ]
-
+    
+    # check cols
+    assertthat::assert_that(all(unique(data_fun_response$variable) == response))
     return(data_fun_response)
   }
-
-
-#### function for functional response of X in relation to levels of Y and Z
-
-# need to select which layers are drivers, options are
-# klepts, foragers, all agents (already covered), and items
-# two of 4 possible per time
-# response is always 3 vars -- pc intake of foragers, klepts and all
