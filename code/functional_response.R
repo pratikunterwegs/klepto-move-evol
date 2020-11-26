9b10b896c2d2219c96d87a7da838069eda0bb66e
P.R. Gupte <P284074@Workspace.rug.nl>
2020-11-26 13:43:42 +0100

    add fun to read data

--- code/functional_response.R
+++ code/functional_response.R
@@ -4,6 +4,84 @@ floor_any <- function(x, v) {
   floor(x / v) * v
 }
 
+#' Read in simulation data.
+#'
+#' @param data_folder Folder to read from.
+#' @param which_gen Generations to read. Numeric sequence.
+#' @param n_time Number of timesteps. Single numeric.
+#'
+#' @return A data.table with per capita intake.
+#' @export
+do_read_data <- function(
+  data_folder,
+  which_gen = seq(991, 998, 1),
+  n_time = 400
+) {
+  # list files
+  data_files <- data.table::CJ(
+    which_gen,
+    layers
+  )
+  data_files$filepath <- glue::glue_data(
+    .x = data_files,
+    "{data_folder}/{which_gen}{layers}.txt"
+  )
+  # split by layer
+  data_files <- split(data_files, data_files$layers)
+  
+  # get only filepaths
+  data_files <- lapply(data_files, `[[`, "filepath")
+  
+  # now read in all files as matrices
+  data_in <- rapply(object = data_files, function(file_list) {
+    matrices <- lapply(as.list(file_list), function(fl) {
+      tseries::read.matrix(fl)
+    })
+    
+    # sum the agents over the generations 991 -- 998
+    matrices <- Reduce(f = `+`, x = matrices)
+  }, how = "list")
+  
+  # convert to dataframe for capacity wise mean
+  data_proc <- rapply(data_in, function(matrix_) {
+    vals <- as.vector(matrix_) / length(which_gen) # for N gen mean
+    vals <- vals / n_time # for timestep mean
+    
+    return(vals)
+  }, how = "list")
+  
+  # get per capita forager intake
+  pc_intake_forager <- data_proc[["foragers_intake"]] /
+    data_proc[["foragers"]]
+  
+  # get per capita klepto intake
+  pc_intake_klepts <- data_proc[["klepts_intake"]] /
+    data_proc[["klepts"]]
+  
+  # replace NANs with 0
+  pc_intake_forager[is.nan(pc_intake_forager)] <- 0
+  pc_intake_klepts[is.nan(pc_intake_klepts)] <- 0
+  
+  # total pc intake
+  pc_intake_total <- pc_intake_forager + pc_intake_klepts
+  
+  # add pc intake to list
+  data_proc <- append(data_proc, list(
+    pc_intake_forager = pc_intake_forager,
+    pc_intake_klepts = pc_intake_klepts,
+    pc_intake_total = pc_intake_total
+  ))
+  
+  #### get pc intake per items and per agents ####
+  # first make data.table
+  data_proc <- as.data.table(data_proc)
+  
+  # get total agents
+  data_proc[, `:=`(total_agents = foragers + klepts)]
+  
+  return(data_proc)
+}
+
 #### functions to get the functional response in kleptomove ####
 
 #' Get generalised functional response data.
