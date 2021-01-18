32b2998c5e154d76b3a3560d266404165423ab98
Pratik Gupte <pratikgupte16@gmail.com>
2021-01-18 14:19:43 +0100

    needs rewrite

--- code/code_getData.r
+++ code/code_getData.r
@@ -7,12 +7,48 @@ library(stringr)
 
 #### list data directories
 # get full names of data containing directories
-data_dirs <- list.dirs(path = "bin/output/modelOutput_klepts_20_09_19",
+data_dirs <- list.dirs(path = "data",
                        full.names = TRUE,
                        recursive = FALSE)
 
 # exclude nonsense directories
 data_dirs <- data_dirs[stringr::str_detect(data_dirs, "sim")]
+
+#list all folders that contain the word "sim"
+lfolders <- list.dirs(path = "data", recursive = FALSE)
+folders <- lfolders[ grepl("sim", lfolders) ]
+
+n_agents = 10000
+n_time = 400
+
+summary_list <- list()
+#generation_dataframe_list <- list()
+
+for(i in seq_along(folders)){
+  source(paste(folders[[i]], "sourceMe.R", sep = "/"))
+  
+  # SUMMARY LIST ---------------------------------------------------------------
+  data_extended <- summary()
+  data_extended <- data.table(data_extended$agents)
+  data_extended[, stealing := (n_agents * n_time) - (foraging+handling)]
+  
+  data <- data_extended[, list(`pop fitness`, foraging, stealing, handling, conflicts)]
+  #data <- as_tibble(data)
+  
+  data[, gen := seq(nrow(data))]
+  
+  # remove last gen
+  data = data[gen < max(gen), ]
+  setnames(data, old = "pop fitness", new = "pop_fitness")
+  # melt
+  data = melt(data, id.vars = c("gen", "pop_fitness", "conflicts"))
+  
+  data[, value := value / sum(value), by = "gen"]
+  
+  summary_list[[i]] <- data
+  
+}
+
 
 # write a function to run sourceMe.r and get summary data
 funcGetData <- function(filepath){
@@ -31,7 +67,7 @@ funcGetData <- function(filepath){
   # get summary data
   temp_summary <- summary() # who decided to name this after a common R func?
   # get agent data
-  temp_summary <- data.table(temp_summary$agents)
+  temp_summary <- as.data.table(temp_summary$agents)
   
   # get sim identifier
   sim <- as.numeric(stringr::str_split(filepath, "sim")[[1]][2])
