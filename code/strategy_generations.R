669b85e42a39113148e734c5ac4cb4643620e9ff
Pratik Gupte <pratikgupte16@gmail.com>
2021-01-19 13:50:55 +0100

    style files

--- code/strategy_generations.R
+++ code/strategy_generations.R
@@ -7,21 +7,29 @@ data <- fread("data/data_strategy_generations.csv")
 
 # relevel factors
 data$strategy <- factor(data$strategy,
-                        levels = c("forg", "fixd", "flex"),
-                        labels = c("foraging only",
-                                   "fixed strategy",
-                                   "flexible strategies"))
+  levels = c("forg", "fixd", "flex"),
+  labels = c(
+    "foraging only",
+    "fixed strategy",
+    "flexible strategies"
+  )
+)
 
-ggplot(data[strategy == "fixed strategy",])+
-  geom_line(aes(gen, value, 
-                col = variable,
-                group = interaction(variable, strategy, replicate)))+
-  facet_grid(~strategy)+
-  scale_colour_manual(values = c("blue", "forestgreen", "red"))+
-  theme_bw()+
-  theme(legend.position = "top")+
-  labs(x = "generation",
-       y = "p(strategy) in population")
+ggplot(data[strategy == "fixed strategy", ]) +
+  geom_line(aes(gen, value,
+    col = variable,
+    group = interaction(variable, strategy, replicate)
+  )) +
+  facet_grid(~strategy) +
+  scale_colour_manual(values = c("blue", "forestgreen", "red")) +
+  theme_bw() +
+  theme(legend.position = "top") +
+  labs(
+    x = "generation",
+    y = "p(strategy) in population"
+  )
 
-ggsave(filename = "figures/fig_strategy_generations.png",
-       dpi = 300, width = 8, height = 4)
+ggsave(
+  filename = "figures/fig_strategy_generations.png",
+  dpi = 300, width = 8, height = 4
+)
