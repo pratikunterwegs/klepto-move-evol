e960c2de8bdaf6aa86df5f76b380bd2b501ae219
Pratik Gupte <pratikgupte16@gmail.com>
2021-02-07 20:37:26 +0100

    edit function to not use special symbol

--- R/fun_get_strategy_gen.r
+++ R/fun_get_strategy_gen.r
@@ -7,7 +7,9 @@
 #' @return A data.table of strategy counts over generations.
 #' @export
 
-get_strategy_gen <- function(filepath) {
+get_strategy_gen <- function(filepath,
+                             n_agents = 10000,
+                             n_time = 400) {
 
   # what to source
   toSource <- glue::glue("{filepath}/sourceMe.R")
@@ -15,29 +17,38 @@ get_strategy_gen <- function(filepath) {
 
   # get summary
   data <- summary()
-  data <- data.table::as.data.table(data$agents)
+  data = data[["agents"]]
+  data = as.data.frame(data)
+  data.table::setDT(data)
 
   # how many agents and timesteps
-  n_agents <- config$agents.N
-  n_time <- config$T
+  # n_agents <- config$agents.N
+  # n_time <- config$T
+
+  assertthat::assert_that(is.data.table(data),
+                          msg = "strategy_gen: not a data table")
+
+  assertthat::assert_that(nrow(data) > 1,
+                          msg = "strategy_gen: empty datatable")
 
   # get stealing
-  data[, stealing := (n_agents * n_time) - (foraging + handling)]
-
-  data <- data[, list(
-    `pop fitness`, foraging,
-    stealing, handling, conflicts
+  data$stealing = (n_agents * n_time) - (data$foraging + data$handling)
+  
+  data <- data[, c(
+    "pop fitness", "foraging",
+    "stealing", "handling", "conflicts"
   )]
 
-  data[, gen := seq(nrow(data))]
+  data$gen = seq(nrow(data))
 
   # remove last gen
-  data <- data[gen < max(gen), ]
+  data <- data[data$gen < max(data$gen), ]
   data.table::setnames(data, old = "pop fitness", new = "pop_fitness")
   # melt
   data <- data.table::melt(data, id.vars = c("gen", "pop_fitness", "conflicts"))
 
-  data[, value := value / sum(value), by = "gen"]
+  # data[, value := value / sum(value), by = "gen"]
+  data$value = data$value / (n_agents * n_time)
 
   return(data)
 }
