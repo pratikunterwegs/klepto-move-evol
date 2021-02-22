823284d827774483daa2d6ebc61de86ee1cbb812
Pratik Gupte <pratikgupte16@gmail.com>
2021-02-22 11:04:26 +0100

    add landscape reader and slope fun

---
+++ R/fun_read_land.R
@@ -0,0 +1,72 @@
+
+#' Read landscape data from PNG output.
+#'
+#' @param landscape_file Path to the landscape file.
+#' @param layer Which layer to read.
+#' @param crop_dim How many rows and columns to return.
+#' @param type What to return, either "items" for the item counts, or "gradient"
+#' for the gradient in item counts.
+#'
+#' @return A data.table of X, Y and value
+#' @export
+read_landscape <- function(landscape_file, layer, crop_dim,
+                           type = c("items", "gradient")) {
+
+  # read the layer-th layer
+  land <- png::readPNG(landscape_file)[, , layer]
+  # crop to square matrix of size dim
+  land <- land[seq(crop_dim), seq(crop_dim)]
+
+  if (type == "items") {
+    # make data.table
+    land <- data.table::as.data.table(land)
+    # set names
+    setnames(land, as.character(seq(crop_dim)))
+    # add x col
+    land$x <- seq(crop_dim)
+    # melt and make long
+    land <- data.table::melt(land,
+      id.vars = "x",
+      variable.name = "y",
+      value.name = "items"
+    )
+
+    # make x and y numeric
+    land[, c("x", "y") := list(
+      as.numeric(x),
+      as.numeric(y)
+    )]
+    # return
+    return(land)
+  } else if (type == "gradient") {
+    # get slope in X and Y
+    grad_matrix <- pracma::gradient(land, seq(crop_dim), seq(crop_dim))
+    grad_x <- grad_matrix$X
+    grad_y <- grad_matrix$Y
+
+    # get slope magnitude overall
+    slope <- t(sqrt((grad_x^2) + (grad_y^2)))
+
+    # convert to data.table
+    slope <- data.table::as.data.table(slope)
+
+    setnames(slope, as.character(seq(crop_dim)))
+    # add x col
+    slope$x <- seq(crop_dim)
+    # melt and make long
+    slope <- data.table::melt(slope,
+      id.vars = "x",
+      variable.name = "y",
+      value.name = "gradient"
+    )
+
+    # make x and y numeric
+    slope[, c("x", "y") := list(
+      as.numeric(x),
+      as.numeric(y)
+    )]
+
+    # return
+    return(slope)
+  }
+}
