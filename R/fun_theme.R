3b67c9aad85fda325089103ad7d76eae03c5bfde
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-01 10:53:33 +0100

    better theme

--- R/fun_theme.R
+++ R/fun_theme.R
@@ -5,11 +5,15 @@
 #'
 #' @return None
 #' @export
-theme_custom <- function(grid = FALSE, ...) {
+theme_custom <- function(grid = FALSE, 
+                         landscape = FALSE,
+                         ...) {
   this_theme <- ggplot2::theme_test(...) +
     ggplot2::theme(
       legend.position = "none",
+      axis.title = ggplot2::element_text(face = "bold"),
       axis.ticks.length = ggplot2::unit(1.5, units = "mm"),
+      axis.text.y = ggplot2::element_text(angle = 90, hjust = 0.5),
       # axis.line = ggplot2::element_line(
       #   colour = "black",
       #   size = 0.3),
@@ -28,8 +32,19 @@ theme_custom <- function(grid = FALSE, ...) {
       ggplot2::theme(
         panel.grid.major = ggplot2::element_line(
           colour = "grey70",
-          size = 0.2
+          size = 0.1
         )
+      )
+  }
+  
+  if (landscape) {
+    this_theme = this_theme +
+      ggplot2::theme(
+        legend.position = "top",
+        legend.key.height = ggplot2::unit(1, units = "mm"),
+        axis.text = ggplot2::element_blank(),
+        axis.text.y = ggplot2::element_blank(),
+        axis.title = ggplot2::element_blank()
       )
   }
 
