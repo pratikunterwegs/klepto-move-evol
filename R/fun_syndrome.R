233337c9de29391c43b7529e1409d04c473586dc
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-03 17:04:42 +0100

    fix fun

--- R/fun_syndrome.R
+++ R/fun_syndrome.R
@@ -1,44 +1,43 @@
 get_pref_handler_by_strat <- function(
-    data_folder,
-    generations,
-    folder,
-    gen_data, 
-    weight_klept_bias,
-    weight_handler_pref) {
+                                      data_folder,
+                                      generations,
+                                      folder,
+                                      gen_data,
+                                      weight_klept_bias,
+                                      weight_handler_pref) {
 
   # source 'source_me.R' in the data folder
   source(sprintf("%s/sourceMe.R", data_folder))
 
   # get generation objects across generations
-  weight_data_gen = lapply(generations, function(g), {
-      # get generation object
-      G = generation(g - 1)
-      
-      # get the two weights we want
-      # why do we multiply by 20, who knows
-      weights = G[["agents"]][["ann"]][, c(weight_klept_bias, weight_handler_pref)] * 20
-      
-      # set names
-      colnames(weights) = c("klept_bias", "handler_pref")
+  weight_data_gen <- lapply(generations, function(g) {
+    # get generation object
+    G <- generation(g - 1)
 
-      # this is a matrix, discretise the klept bias and handler pref 1,1 is forager, has handler preference
-      weights = weights > 0
+    # get the two weights we want
+    # why do we multiply by 20, who knows
+    weights <- G[["agents"]][["ann"]][, c(weight_klept_bias, weight_handler_pref)] * 20
 
-      # table the weights, there will be four combinations
-      weight_table = table(weights)
+    # set names
+    colnames(weights) <- c("klept_bias", "handler_pref")
 
-      # convert to data.table
-      weight_table = data.table::as.data.table(weight_table)
+    # this is a matrix, discretise the klept bias and handler pref 1,1 is forager, has handler preference
+    weights <- weights > 0
 
-      # add generation
-      weight_table$gen = g
+    # table the weights, there will be four combinations
+    weight_table <- table(weights)
 
-      return(weight_table)
-    }
-  )
+    # convert to data.table
+    weight_table <- data.table::as.data.table(weight_table)
+
+    # add generation
+    weight_table$gen <- g
+
+    return(weight_table)
+  })
 
   # bind all the data together
-  weight_data = data.table::rbindlist(weight_data_gen)
+  weight_data <- data.table::rbindlist(weight_data_gen)
 
   return(weight_data)
 }
