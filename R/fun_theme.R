b9b0ecc3aa845f5e65c6c611bba49c851028ab22
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-04 17:49:41 +0100

    add strip theme

--- R/fun_theme.R
+++ R/fun_theme.R
@@ -12,16 +12,18 @@ theme_custom <- function(grid = FALSE,
     ggplot2::theme(
       legend.position = "none",
       axis.title = ggplot2::element_text(face = "bold"),
-      axis.ticks.length = ggplot2::unit(1.5, units = "mm"),
+      # axis.ticks.length = ggplot2::unit(1.5, units = "mm"),
       axis.text.y = ggplot2::element_text(angle = 90, hjust = 0.5),
       # axis.line = ggplot2::element_line(
       #   colour = "black",
       #   size = 0.3),
-      strip.background = ggplot2::element_rect(
-        colour = "grey20",
-        fill = "lightgrey",
-        size = 0.3
-      ),
+      # strip.background = ggplot2::element_rect(
+      #   colour = "grey20",
+      #   fill = "lightgrey",
+      #   size = 0.3
+      # ),
+      strip.background = ggplot2::element_blank(),
+      strip.text = ggplot2::element_text(face = "bold"),
       panel.background = ggplot2::element_rect(
         fill = "grey99"
       )
