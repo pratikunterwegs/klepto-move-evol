25acb567200c64162f98d7d9a5d4169cceccd347
Pratik Gupte <pratikgupte16@gmail.com>
2021-05-04 13:56:40 +0200

    no copy tex

--- copy_tex.sh
+++ copy_tex.sh
@@ -6,10 +6,10 @@
 # Rscript --slave -e 'lapply(list.files(pattern = "\\d{2}\\w+.Rmd"), function(x) knitr::purl(x, output = sprintf("R/%s", gsub(".{4}$", ".R", x))))'
 
 # copy pdfs and name correctly
-rm docs/ms_kleptomove_*.tex
+# rm docs/ms_kleptomove_*.tex
 rm docs/ms_kleptomove_*.pdf
 rm docs/supplement_kleptomove_*.pdf
-cp -p overleaf-kleptomove/manuscript.tex docs/ms_kleptomove_`date -I`.tex
+# cp -p overleaf-kleptomove/manuscript.tex docs/ms_kleptomove_`date -I`.tex
 cp -p overleaf-kleptomove/manuscript.pdf docs/ms_kleptomove_`date -I`.pdf
 cp -p supplementary_material/supplement.pdf docs/supplement_kleptomove_`date -I`.pdf
 cp figures/fig_0*.png overleaf-kleptomove/figures
