b538beb365f98c5cefef3c05fb7acc3c165e022b
Pratik Gupte <pratikgupte16@gmail.com>
2021-07-19 10:33:53 +0200

    minor changes

--- bash/copy_tex.sh
+++ bash/copy_tex.sh
@@ -2,10 +2,8 @@
 
 # copy pdfs and name correctly
 rm docs/ms_kleptomove_*.pdf
-rm docs/supplement_kleptomove_*.pdf
 
 cp -p overleaf-kleptomove/manuscript.pdf docs/ms_kleptomove_`date -I`.pdf
-cp -p supplementary_material/supplement.pdf docs/supplement_kleptomove_`date -I`.pdf
 cp figures/fig_0*.png overleaf-kleptomove/figures
 
 # render docx
