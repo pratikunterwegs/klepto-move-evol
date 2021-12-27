a4c901916bf79c9b0eb25c3563f7f8f133f67459
Pratik Gupte <pratikgupte16@gmail.com>
2021-12-27 12:03:10 +0100

    add edits

--- bash/copy_tex.sh
+++ bash/copy_tex.sh
@@ -7,7 +7,7 @@ cp -p overleaf-kleptomove/manuscript.pdf docs/ms_kleptomove_`date -I`.pdf
 cp figures/fig_0*.png overleaf-kleptomove/figures
 
 # render docx
-pandoc overleaf-kleptomove/manuscript.tex --reference-doc=docs/template.docx --bibliography=overleaf-kleptomove/kleptomove.bib -o docs/ms_kleptomove_`date -I`.docx
+# pandoc overleaf-kleptomove/manuscript.tex --reference-doc=docs/template.docx --bibliography=overleaf-kleptomove/kleptomove.bib -o docs/ms_kleptomove_`date -I`.docx
 
 # no refs
-pandoc overleaf-kleptomove/manuscript.tex --reference-doc=docs/template.docx -o docs/ms_kleptomove_`date -I`_no_refs.docx
+# pandoc overleaf-kleptomove/manuscript.tex --reference-doc=docs/template.docx -o docs/ms_kleptomove_`date -I`_no_refs.docx
