3780c64e9a6b9b62e23d352eb9c3cacfaf720e4b
Pratik Gupte <pratikgupte16@gmail.com>
2022-01-18 20:18:40 +0100

    update copy tex

--- bash/copy_tex.sh
+++ bash/copy_tex.sh
@@ -3,8 +3,17 @@
 # copy pdfs and name correctly
 rm docs/ms_kleptomove_*.pdf
 
-cp -p overleaf-kleptomove/manuscript.pdf docs/ms_kleptomove_`date -I`.pdf
 cp figures/fig_0*.png overleaf-kleptomove/figures
+
+cd overleaf-kleptomove
+pdflatex manuscript.tex
+bibtex manuscript.aux
+bibtex manuscript.aux
+pdflatex manuscript.tex
+
+cd ..
+
+cp -p overleaf-kleptomove/manuscript.pdf docs/ms_kleptomove_`date -I`.pdf
 
 # render docx
 # pandoc overleaf-kleptomove/manuscript.tex --reference-doc=docs/template.docx --bibliography=overleaf-kleptomove/kleptomove.bib -o docs/ms_kleptomove_`date -I`.docx
